2cdd0e0d7c294a34a02a221c87260ce957513350
RogerCastillo877 <roger.castillo877@gmail.com>
2022-04-04 17:17:56 -0500

    Get geolocalization

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if( !navigator.geolocation ) {
+  alert('Navegador no soporta la geolocalización');
+  throw new Error('T he browser does not support Geolocalization');
+}
+
 if (environment.production) {
   enableProdMode();
 }
