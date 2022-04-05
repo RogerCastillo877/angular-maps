482305afe015ec15337361419629347de4cbbdb5
RogerCastillo877 <roger.castillo877@gmail.com>
2022-04-05 18:21:56 -0500

    Http request to get places

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +8,14 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   public useLocation?: [number, number];
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
 
   get isUserLocationReady(): boolean {
     return !!this.useLocation;
   }
 
-  constructor() {
+  constructor( private http: HttpClient) {
     this.getUserLocation();
   }
 
@@ -32,4 +36,16 @@ export class PlacesService {
       )
     });
   };
+
+  getPlacesByQuery( query: string = '') {
+
+    this.isLoadingPlaces = true;
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-74.11864145718255%2C4.700927314889938&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1Ijoicm9jYXM4NyIsImEiOiJja3Y5a3JtODVhNWlqMzJuenU3d3JtbTVsIn0.zI5FNRvuVM_PyFE4rBYfDw`)
+      .subscribe( resp => {
+        console.log(resp.features);
+        this.isLoadingPlaces = false;
+        this.places = resp.features;        
+      });
+  }
 }
